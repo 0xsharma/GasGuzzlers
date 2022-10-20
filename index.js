37c879779acc72dbc82aa2f9587625a8d19e7e45
Shivam Sharma <shivam691999@gmail.com>
2022-10-20 14:39:01 +0530

    chg : format script

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ var web3 = new Web3(Web3.givenProvider || WSWEB3);
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
-async function main(){
+async function main() {
 
     endblock = await web3.eth.getBlockNumber()
     startBlock = endblock - 5000;
     var contractMapTo = new Map();
     var contractMapGasUsed = new Map();
 
-    for (let i = startBlock; i<endblock ; i++) {
+    for (let i = startBlock; i < endblock; i++) {
 
         try {
             await web3.eth.net.isListening()
         } catch (error) {
-            console.log('ERR : ' + error+'\n')
+            console.log('ERR : ' + error + '\n')
             await timer(1500)
             web3 = new Web3(Web3.givenProvider || WSWEB3);
             continue
@@ -27,28 +27,28 @@ async function main(){
 
         try {
             console.log("Block: " + i);
-        var blockObject = await web3.eth.getBlock(i);
-        var allTransactions = blockObject.transactions;
-        for (let j = 0; j < allTransactions.length; j++) {
-            var transaction = allTransactions[j];
-            var transactionObject = await web3.eth.getTransactionReceipt(transaction);
-            var gasUsed = transactionObject.gasUsed;
-            var tx_to = transactionObject.to;
-            if (contractMapTo.has(tx_to)) {
-                let gasUsedValue = parseInt(contractMapGasUsed.get(tx_to));
-                gasUsedValue += parseInt(gasUsed);
-                contractMapGasUsed.set(tx_to, gasUsedValue);
-                contractMapTo.set(tx_to, contractMapTo.get(tx_to) + 1);
-            }else{
-                contractMapTo.set(tx_to, 1);
-                contractMapGasUsed.set(tx_to, parseInt(gasUsed));
+            var blockObject = await web3.eth.getBlock(i);
+            var allTransactions = blockObject.transactions;
+            for (let j = 0; j < allTransactions.length; j++) {
+                var transaction = allTransactions[j];
+                var transactionObject = await web3.eth.getTransactionReceipt(transaction);
+                var gasUsed = transactionObject.gasUsed;
+                var tx_to = transactionObject.to;
+                if (contractMapTo.has(tx_to)) {
+                    let gasUsedValue = parseInt(contractMapGasUsed.get(tx_to));
+                    gasUsedValue += parseInt(gasUsed);
+                    contractMapGasUsed.set(tx_to, gasUsedValue);
+                    contractMapTo.set(tx_to, contractMapTo.get(tx_to) + 1);
+                } else {
+                    contractMapTo.set(tx_to, 1);
+                    contractMapGasUsed.set(tx_to, parseInt(gasUsed));
+                }
             }
-        }
-           
+
         } catch (error) {
             continue
         }
-        
+
     }
 
     let now = Math.floor(new Date().getTime() / 1000)
@@ -56,7 +56,7 @@ async function main(){
     console.log(contractMapTo)
     console.log(contractMapGasUsed)
 
-    await fs.appendFile(`./output/out-${now}.csv`, `contract, txs, gasUsed` , function (err) {
+    await fs.appendFile(`./output/out-${now}.csv`, `contract, txs, gasUsed`, function (err) {
         if (err) throw err;
         console.log('Created output file : ' + `./output/out-${now}.csv`);
     });
@@ -65,9 +65,9 @@ async function main(){
 
 
     for (var [key, value] of contractMapTo.entries()) {
-        await fs.appendFile(`./output/out-${now}.csv`, `\n${key}, ${value}, ${contractMapGasUsed.get(key)}` , function (err) {
+        await fs.appendFile(`./output/out-${now}.csv`, `\n${key}, ${value}, ${contractMapGasUsed.get(key)}`, function (err) {
             if (err) throw err;
-            
+
         });
     }
 
